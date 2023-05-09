f86bfda22a844b664e685e27e9a1988dec8a9f19
ProjetosMariaisadora <100208644+ProjetosMariaisadora@users.noreply.github.com>
2023-05-09 12:12:46 -0300

    Add files via upload

---
+++ Exemplo_01/teste.js
@@ -0,0 +1,56 @@
+var n1 = parseInt (prompt("Digite o N1:"))
+var n2 = parseInt (prompt("Digite o N2:"))
+console.log("N1 é:" + typeof n1)
+console.log("N2 é:"+ typeof n2)
+
+var soma = n1 + n2
+console.log("Soma é: " + typeof soma)
+
+if (soma>10){
+    document.write(`<p class="menor">A soma de ${n1} com ${n2} é ${soma}</p>`)
+}
+else{
+    document.write(`<p class="menor">A soma de ${n1} com ${n2} é ${soma}</p>`)
+}
+
+
+
+
+
+
+
+
+
+//var nome = prompt("Digite seu nome:")
+ //alert("Bom dia" + nome) 
+ // TEMPLATE STRING => INTERPOLAÇÃO
+ //alert(`Bom dia ${nome}`)
+ 
+ //alert("oi")
+/*
+*/
+
+ //console.log(nome)
+ //document.write(nome)
+
+ // VARIÁVEIS
+
+
+//let a = 10
+//console.log(a)
+
+//if(true){
+    //let a =15
+    //console.log(a)
+//}
+//console.log(a)
+//-----------------------------//
+//var = global
+//let = local
+//const = sempre constante
+
+
+
+
+
+
